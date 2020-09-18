7a6486b30fdc4670951107c557ebfaba4bfbef8c
Puria Nafisi Azizi <puria@dyne.org>
2020-09-18 20:04:47 +0200

    🥠 Filter just .zen contracts

--- packages/ui/src/utils.js
+++ packages/ui/src/utils.js
@@ -1,19 +1,7 @@
 import { ZENCODE_DIR } from "@restroom-mw/utils";
 import { promises as fs } from "fs";
 import { parse, relative, resolve } from "path";
-
-// https://qwtel.com/posts/software/async-generators-in-the-wild/
-async function* getFiles(rootPath) {
-  const fileNames = await fs.readdir(rootPath);
-  for (const fileName of fileNames) {
-    const path = resolve(rootPath, fileName);
-    if ((await fs.stat(path)).isDirectory()) {
-      yield* getFiles(path);
-    } else {
-      yield path;
-    }
-  }
-}
+import readdirp from "readdirp";
 
 /**
  * Reads the directory and list all the files
@@ -21,13 +9,14 @@ async function* getFiles(rootPath) {
  * @param {string} path for where to look at folders
  * @returns {object}
  */
-export const ls = async (path) => {
-  let files = {};
-  for await (const item of getFiles(path)) {
-    const p = relative(ZENCODE_DIR, item).split(".")[0];
-    files[p] = item;
+export const ls = async (root) => {
+  const contracts = {};
+  for await (const contract of readdirp(root, { fileFilter: "*.zen" })) {
+    const { path, fullPath } = contract;
+    const name = path.split(".")[0];
+    contracts[name] = fullPath;
   }
-  return files;
+  return contracts;
 };
 
 export const nl2br = (str) => str.replace(/(?:\r\n|\r|\n)/g, "  \n");
