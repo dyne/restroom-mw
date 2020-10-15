f8af9488d0719d50796f1c613b91c2d32cd0f3c8
Puria Nafisi Azizi <puria@dyne.org>
2020-10-15 20:46:24 +0200

    feat: ✨ Implements and close #5

--- packages/db/src/index.js
+++ packages/db/src/index.js
@@ -2,17 +2,18 @@ import { Sequelize } from "sequelize";
 import { Restroom } from "@restroom-mw/core";
 
 /**
- * **CONNECT** `I have a database connection uri at {}`
- *
- * **SELECT** `I select {} from {} where {}`
+ * **CONNECT** `I have a database connection named {}`
  *
  * **SAVE** `save the result into the database`
+ *
+ * **SAVE_WITH_TABLENAME**: `save the result into the database within the table {}`
  * @constant
  * @default
  */
 const ACTIONS = {
   CONNECT: "I have a database connection named {}",
   SAVE: "save the result into the database",
+  SAVE_WITH_TABLENAME: "save the result into the database within the table {}",
 };
 
 export default (req, res, next) => {
@@ -34,6 +35,17 @@ export default (req, res, next) => {
       const db = new Sequelize(dbUrl);
       const resultTable = db.define("results", { result: Sequelize.TEXT });
       await db.sync();
+      await resultTable.create({ result });
+    }
+
+    if (zencode.match(ACTIONS.SAVE_WITH_TABLENAME)) {
+      const db = new Sequelize(dbUrl);
+      const [table_name] = zencode.paramsOf(ACTIONS.SAVE_WITH_TABLENAME);
+      const resultTable = db.define(
+        table_name,
+        { result: Sequelize.TEXT },
+        { freezeTableName: true }
+      );
       await resultTable.create({ result });
     }
   });
--- packages/db/tests/index.js
+++ packages/db/tests/index.js
@@ -1,7 +1,9 @@
 import test from "ava";
 import request from "supertest";
 import express from "express";
+import { QueryTypes, Sequelize } from "sequelize";
 import bodyParser from "body-parser";
+import fs from "fs";
 
 process.env.ZENCODE_DIR = "./test/fixtures";
 const db = require("..").default;
@@ -16,12 +18,49 @@ test("Middleware db should correctly work and save data", async (t) => {
   app.use(bodyParser.json());
   app.use(db);
   app.use("/*", zencode);
-
-  const res = await request(app)
-    .post("/database")
-    .send({ data: { sqlite: "sqlite://./database.test" } });
+  const data = { data: { sqlite: "sqlite://./test/database.test" } };
+  const res = await request(app).post("/database").send(data);
 
   t.is(res.status, 200);
   t.true(Array.isArray(res.body.array));
   t.is(res.body.array.length, 5);
+  const sequelize = new Sequelize(data.data.sqlite);
+  const saved_result = await sequelize.query("select result from results", {
+    type: QueryTypes.SELECT,
+  });
+  t.deepEqual(res.body, JSON.parse(saved_result[0].result));
+  t.teardown(() => {
+    fs.unlinkSync("./test/database.test");
+  });
+});
+
+test("Middleware db should correctly work and save data with given tablename", async (t) => {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(db);
+  app.use("/*", zencode);
+
+  const data = {
+    data: {
+      sqlite: "sqlite://./test/database_tablename.test",
+      tablename: "tablename",
+    },
+  };
+  const sequelize = new Sequelize(data.data.sqlite);
+  const sql =
+    "CREATE TABLE IF NOT EXISTS `tablename` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `result` TEXT, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL);";
+  await sequelize.query(sql);
+  const res = await request(app).post("/database_table").send(data);
+
+  const saved_result = await sequelize.query("select result from tablename", {
+    type: QueryTypes.SELECT,
+  });
+
+  t.is(res.status, 200);
+  t.true(Array.isArray(res.body.array));
+  t.is(res.body.array.length, 5);
+  t.deepEqual(res.body, JSON.parse(saved_result[0].result));
+  t.teardown(() => {
+    fs.unlinkSync("./test/database_tablename.test");
+  });
 });
