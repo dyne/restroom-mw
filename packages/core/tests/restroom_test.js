a9906387d6502d8e33569658cc59b49650ef92ff
Puria Nafisi Azizi <puria@dyne.org>
2020-10-16 20:53:51 +0200

    test: 🚨 Add more test cover broken zencodes

--- packages/core/tests/restroom_test.js
+++ packages/core/tests/restroom_test.js
@@ -1,1 +1,15 @@
 import test from "ava";
+import request from "supertest";
+import express from "express";
+import bodyParser from "body-parser";
+
+process.env.ZENCODE_DIR = "./test/fixtures";
+const zencode = require("..").default;
+
+test("Should correctly fail", async (t) => {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use("/*", zencode);
+  const res = await request(app).post("/broken").send({ data: {} });
+  t.is(res.status, 500);
+});
