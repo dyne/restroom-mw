c752f609426d613f953a4ce5c08ddfee3f7deed3
Puria Nafisi Azizi <puria@dyne.org>
2020-10-18 12:58:45 +0200

    test(core): 🚨 testing the utils (mostly 404 handling)

---
+++ packages/core/tests/utils_custom_test.js
@@ -0,0 +1,16 @@
+import test from "ava";
+import request from "supertest";
+import express from "express";
+import bodyParser from "body-parser";
+
+process.env.ZENCODE_DIR = "./test/not_existent";
+const zencode = require("../../core").default;
+
+test("getMessages show custom message", async (t) => {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use("/*", zencode);
+
+  const res = await request(app).post("/non-existent-endpoint");
+  t.is(res.text, "<h2>404: This contract does not exists</h2>");
+});
