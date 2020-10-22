87e867e9b67131ac8bd8f462c290c764f292787b
seabassdk <s.blichfeldt@live.com>
2020-10-22 08:47:47 +0200

    endpoint http requests with revision and using params

--- packages/endpoint/endpoint.js
+++ packages/endpoint/endpoint.js
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import axios from 'axios';
-import RestroomCore from "@restroom-mw/core";
-const { Restroom } = RestroomCore;
+import { Restroom } from "@restroom-mw/core";
 
 const ACTIONS = {
   EXTERNAL_CONNECTION: "that I have an endpoint named {}",
@@ -11,73 +10,53 @@ const ACTIONS = {
 
 export default (req, res, next) => {
   const rr = new Restroom(req, res);
+  const outputName = 'output';
+  let keysContent;
+  let dataContent;
   let externalSourceKeys = [];
-  let externalSourceValue = [];
-
-  let outputName = 'output';
-  let outputValue;
-
-  //get the api call from restroom that contains username and contract name
-  const originalUrl = rr._req.originalUrl;
-  const userContractDir = originalUrl.split('/api/')[1];
-
-  //Get the directories for keys and data
-  const completeDir = process.cwd() + '/contracts/' + userContractDir;
-  const keyFile = completeDir + '.keys';
-  const dataFile = completeDir + '.data';
 
   rr.onBefore(async (params) => {
-
-    const { zencode, data } = params;
+    const { zencode, data, keys } = params;
+    //used in rr.onSuccess
+    keysContent = keys;
+    dataContent = data;
 
     if (zencode.match(ACTIONS.EXTERNAL_CONNECTION)) {
       externalSourceKeys = zencode.paramsOf(ACTIONS.EXTERNAL_CONNECTION);
     }
 
     if (zencode.match(ACTIONS.EXTERNAL_OUTPUT)) {
-
       if (externalSourceKeys.length > 0) {
+        let ouputKeyValue;
+        let outputDataValue;
 
-        //if exists get the value for the external source in the keys or data files that have the externalSourceKey key
-        if (fs.existsSync(keyFile)) {
-          const keyFileContent = JSON.parse(fs.readFileSync(keyFile).toString());
-          for (const key of externalSourceKeys) {
-            // use function getValueForKey to recursively search the json for the key and get the value
-            const value = getValueForKey(keyFileContent, key);
-            if (value) {
-              try {
-                // make the api call with url in value
-                const response = await axios.get(value);
-                outputValue = response.data;
-              } catch (error) {
-                outputValue = null;
-              }
-              rr.setData(outputName, outputValue);
+        for (const key of externalSourceKeys) {
+          // use function getKey to recursively search for the key to get its value
+          const keyValue = getKey(keys, key);
+          const dataValue = getKey(data, key);
+
+          if (keyValue) {
+            try {
+              // make the api call with the keys key value url
+              ouputKeyValue = await axios.get(keyValue).data;
+            } catch (error) {
+              ouputKeyValue = null;
             }
-          };
-        }
+            rr.setData(outputName, ouputKeyValue);
+          }
 
-
-        if (fs.existsSync(dataFile)) {
-          const dataFileContent = JSON.parse(fs.readFileSync(dataFile).toString());
-          for (const key of externalSourceKeys) {
-            const value = getValueForKey(dataFileContent, key);
-            if (value) {
-              try {
-                const response = await axios.get(value);
-                outputValue = response.data;
-              } catch (error) {
-                outputValue = null;
-              }
-              rr.setData(outputName, outputValue);
+          if (dataValue) {
+            try {
+              // make the api call with the data key value url
+              outputDataValue = await axios.get(dataValue).data;
+            } catch (error) {
+              outputDataValue = null;
             }
-          };
+            rr.setData(outputName, outputDataValue);
+          }
         }
-
       }
-
     }
-
   });
 
   rr.onSuccess((args) => {
@@ -85,39 +64,29 @@ export default (req, res, next) => {
 
     if (zencode.match(ACTIONS.PASS_OUTPUT)) {
       const outputNames = zencode.paramsOf(ACTIONS.PASS_OUTPUT);
+      
       if (outputNames.length > 0) {
 
-        //if exists get the value for the external source in the keys or data files with the externalSourceKey key
-        if (fs.existsSync(keyFile)) {
-          const keyFileContent = JSON.parse(fs.readFileSync(keyFile).toString());
-          for (const key of outputNames) {
-            // use function getValueForKey to recursively search the json for the key and get the value
-            const value = getValueForKey(keyFileContent, key);
-            if (value) {
-              try {
-                const response = axios.post(value, JSON.parse(result));
-              } catch (error) {
-                console.log('An error occurred');
-              }
+        for (const key of outputNames) {
+          const keyValue = getKey(keysContent, key);
+          const dataValue = getKey(dataContent, key);
+
+          if (keyValue) {
+            try {
+              axios.post(keyValue, JSON.parse(result));
+            } catch (error) {
+              //could not send the result
             }
-          };
-        }
+          }
 
-
-        if (fs.existsSync(dataFile)) {
-          const dataFileContent = JSON.parse(fs.readFileSync(dataFile).toString());
-          for (const key of outputNames) {
-            const value = getValueForKey(dataFileContent, key);
-            if (value) {
-              try {
-                const response = axios.post(value, JSON.parse(result));
-              } catch (error) {
-                console.log('An error occurred');
-              }
+          if (dataValue) {
+            try {
+              axios.post(dataValue, JSON.parse(result));
+            } catch (error) {
+              //could not send the result
             }
-          };
-        }
-
+          }
+        };
       }
     }
   });
@@ -125,12 +94,27 @@ export default (req, res, next) => {
   next();
 };
 
-// https://stackoverflow.com/questions/15523514/find-by-key-deep-in-a-nested-array
-function getValueForKey(theObject, theKey) {
-  var result = null;
+
+function getKey(obj, theKey, running) {
+  if (!obj || !theKey)
+    return null;
+
+  let result = null;
+  let theObject = null;
+  if (!running) {
+    try {
+      theObject = JSON.parse(obj.toString());
+    } catch (error) {
+      //something went wrong with JSON parsing obj
+    }
+  }
+
+  if (!theObject)
+    return null;
+
   if (theObject instanceof Array) {
     for (var i = 0; i < theObject.length; i++) {
-      result = getKey(theObject[i]);
+      result = getKey(theObject[i], theKey, true);
       if (result) {
         break;
       }
@@ -138,15 +122,18 @@ function getValueForKey(theObject, theKey) {
   }
   else {
     for (var prop in theObject) {
+
       if (prop === theKey) {
         return theObject[prop]
       }
+
       if (theObject[prop] instanceof Object || theObject[prop] instanceof Array) {
-        result = getKey(theObject[prop]);
+        result = getKey(theObject[prop], theKey, true);
         if (result) {
           break;
         }
       }
+
     }
   }
   return result;
