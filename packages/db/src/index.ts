33d9ffa29b701cb5ee3460a4ea133d05f4bb079f
seb <s.blichfeldt@gmail.com>
2020-11-26 18:21:33 +0100

    Updated db middleware with new ACTIONS

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -6,62 +6,206 @@ class Result extends Model {
   public result: string;
 }
 
-/**
- * **CONNECT** `I have a database connection named {}`
- *
- * **SAVE** `save the result into the database`
- *
- * **SAVE_WITH_TABLENAME**: `save the result into the database within the table {}`
- * @constant
- * @default
- */
 const ACTIONS = {
-  CONNECT: "I have a database connection named {}",
-  SAVE: "save the result into the database",
-  SAVE_WITH_TABLENAME: "save the result into the database within the table {}",
+  GET_URI_KEYS: "I have a database uri named {}",
+  GET_TABLE_KEYS: "I have a database table named {}",
+  GET_RECORD: "I read the record {} of the table {} of the database {} and save the result into {}", //done
+  SAVE_OUTPUT: "I save the output into the database {} into the table {}",
+  SAVE_VAR: "I save the {} into the database {} into the table {}"
 };
+
+const parse = (o: string) => {
+  try {
+    return JSON.parse(o);
+  } catch (e) {
+    throw new Error(`[DATABASE]
+      Error in JSON format "${o}"`);
+  }
+};
+
+interface ObjectLiteral {
+  [key: string]: any;
+}
+
+interface QueryGetRecord {
+  id: string,
+  table: string,
+  database: string,
+  varName: string
+}
+
+interface QuerySaveOutput {
+  table: string,
+  database: string
+}
+
+interface QuerySaveVar {
+  varName: string,
+  database: string,
+  table: string,
+}
 
 export default (req: Request, res: Response, next: NextFunction) => {
   try {
     const rr = new Restroom(req, res);
-    let dbUrl: string;
+
+    let keysContent;
+    let dataContent;
+    let content: ObjectLiteral = {};
+    let contentKeys: string[];
+    let dbUriKeys: string[] = [];
+    let tableKeys: string[] = [];
 
     rr.onBefore(async (params) => {
-      const { zencode, data } = params;
-      if (zencode.match(ACTIONS.CONNECT)) {
-        const [connection_name] = zencode.paramsOf(ACTIONS.CONNECT);
-        dbUrl = data[connection_name];
+      const { zencode, keys, data } = params;
+
+      if (zencode.match(ACTIONS.GET_URI_KEYS)) {
+        dbUriKeys = zencode.paramsOf(ACTIONS.GET_URI_KEYS);
       }
+
+      if (zencode.match(ACTIONS.GET_TABLE_KEYS)) {
+        tableKeys = zencode.paramsOf(ACTIONS.GET_TABLE_KEYS);
+      }
+
+      if (zencode.match(ACTIONS.GET_RECORD)) {
+        const dbAllRecordData: string[] = zencode.paramsOf(ACTIONS.GET_RECORD);
+        keysContent =
+          typeof keys === "undefined"
+            ? {}
+            : keys && typeof keys === "object"
+              ? keys
+              : parse(keys);
+        dataContent =
+          typeof data === "undefined"
+            ? {}
+            : data && typeof data === "object"
+              ? data
+              : parse(data);
+        content = { ...dataContent, ...keysContent };
+        contentKeys = Object.keys(content);
+        //create object(s) with the FOUR values of each GET_RECORD
+        const dbQueries: QueryGetRecord[] = [];
+        for (var i = 0; i < dbAllRecordData.length; i += 4) {
+          dbQueries.push({
+            id: dbAllRecordData[i],
+            table: dbAllRecordData[i + 1],
+            database: dbAllRecordData[i + 2],
+            varName: dbAllRecordData[i + 3]
+          });
+        }
+        runChecks(dbQueries, dbUriKeys, contentKeys, 'database');
+        runChecks(dbQueries, tableKeys, contentKeys, 'table');
+        try {
+          for (const query of dbQueries) {
+            const db = new Sequelize(content[query.database]);
+            Result.init(
+              { result: DataTypes.TEXT },
+              { tableName: content[query.table], freezeTableName: true, sequelize: db }
+            );
+            await Result.sync();
+            try {
+              let result = await Result.findByPk(query.id);
+              if (result) {
+                result = result.get({ plain: true })
+                // column name is result
+                const resultData = typeof result['result'] === 'object' ? result['result'] : parse(result['result']);
+                checkForNestedBoolean(resultData);
+                data[query.varName] = resultData;
+              } else {
+                throw new Error(`[DATABASE]
+                      Returned null for id "${query.id}" in table "${query.table}" in db "${query.database}".`);
+              }
+            } catch (e) {
+              throw new Error(`[DATABASE]
+                      Something went wrong for id "${query.id}" in table "${query.table}" in db "${query.database}".`);
+            }
+            db.close();
+          };
+        } catch (e) {
+          throw new Error(`[DATABASE]
+          Databse error: ${e}`);
+        }
+      };
     });
 
-    rr.onSuccess(async (args) => {
+    rr.onSuccess(async (args: { result: any; zencode: any; }) => {
       const { result, zencode } = args;
 
-      if (zencode.match(ACTIONS.SAVE)) {
+      if (zencode.match(ACTIONS.SAVE_OUTPUT)) {
+        const dbAllSaveOutput: string[] = zencode.paramsOf(ACTIONS.SAVE_OUTPUT);
+        const dbQueries: QuerySaveOutput[] = [];
+        //create object(s) with the TWO values in each GET_RECORD
+        for (var i = 0; i < dbAllSaveOutput.length; i += 2) {
+          dbQueries.push({
+            database: dbAllSaveOutput[i],
+            table: dbAllSaveOutput[i + 1],
+          });
+        }
+        //check that table and db are defined in keys or data, and in zencode
+        runChecks(dbQueries, dbUriKeys, contentKeys, 'database');
+        runChecks(dbQueries, tableKeys, contentKeys, 'table');
         try {
-          const db = new Sequelize(dbUrl);
-          Result.init(
-            { result: DataTypes.TEXT },
-            { tableName: "results", sequelize: db }
-          );
-          await Result.sync();
-          await Result.create({ result: result });
+          for (const query of dbQueries) {
+            const db = new Sequelize(content[query.database]);
+            Result.init(
+              { result: DataTypes.TEXT },
+              { tableName: content[query.table], freezeTableName: true, sequelize: db }
+            );
+            await Result.sync();
+            try {
+              //column name must be result
+              await Result.create({ result });
+            } catch (e) {
+              throw new Error(`[DATABASE]
+                    Error in table "${query.table}" in db "${query.database}": ${e}`);
+            }
+            db.close();
+          };
         } catch (e) {
-          throw e;
+          throw new Error(`[DATABASE]
+            Databse error: ${e}`);
         }
       }
-
-      if (zencode.match(ACTIONS.SAVE_WITH_TABLENAME)) {
+      
+      if (zencode.match(ACTIONS.SAVE_VAR)) {
+        const resultObj: any = parse(result);
+        const dbAllSaveVar: string[] = zencode.paramsOf(ACTIONS.SAVE_VAR);
+        const dbQueries: QuerySaveVar[] = [];
+        //create object(s) with the THREE values in each GET_RECORD
+        for (var i = 0; i < dbAllSaveVar.length; i += 3) {
+          dbQueries.push({
+            varName: dbAllSaveVar[i],
+            database: dbAllSaveVar[i + 1],
+            table: dbAllSaveVar[i + 2],
+          });
+        }
+        //check that table and db are defined in keys or data and in zencode
+        runChecks(dbQueries, dbUriKeys, contentKeys, 'database');
+        runChecks(dbQueries, tableKeys, contentKeys, 'table');
         try {
-          const db = new Sequelize(dbUrl);
-          const [table_name] = zencode.paramsOf(ACTIONS.SAVE_WITH_TABLENAME);
-          Result.init(
-            { result: DataTypes.TEXT },
-            { tableName: table_name, freezeTableName: true, sequelize: db }
-          );
-          await Result.create({ result });
+          for (const query of dbQueries) {
+            if (!resultObj[query.varName])
+              throw new Error(`[DATABASE]
+                    Saving Error: Couldn't find variable "${query.varName}" in result.`);
+
+            const db = new Sequelize(content[query.database]);
+            Result.init(
+              { result: DataTypes.TEXT },
+              { tableName: content[query.table], freezeTableName: true, sequelize: db }
+            );
+            await Result.sync();
+            try {
+              //column name must be result
+              await Result.create({ result: JSON.stringify({ [query.varName]: resultObj[query.varName] }) });
+            } catch (e) {
+              throw new Error(`[DATABASE]
+                    Error in table "${query.table}" in db "${query.database}": ${e}`);
+            }
+            db.close();
+          };
         } catch (e) {
-          throw e;
+          throw new Error(`[DATABASE]
+            Databse error: ${e}`);
         }
       }
     });
@@ -69,4 +213,47 @@ export default (req: Request, res: Response, next: NextFunction) => {
   } catch (e) {
     next(e);
   }
+};
+
+const runChecks = (
+  keys: any[],
+  actionKeys: string | any[],
+  contentKeys: string | any[],
+  keyName: string
+) => {
+  keys.forEach((key: any) => {
+    //Check that all enpoints (urlKeys) have been defined using statement EXTERNAL_CONNECTION
+    if (actionKeys.includes(key[keyName]) === false) {
+      throw new Error(`[DATABASE]
+              Error: "${key[keyName]}" has not been defined in zencode, please define it with
+              the following zencode sentence "${ACTIONS.GET_URI_KEYS}" or "${ACTIONS.GET_TABLE_KEYS}" `);
+    }
+
+    // check that all endpoints (urlKeys) are properties in either data or keys
+    if (contentKeys.includes(key[keyName]) === false) {
+      throw new Error(`[DATABASE]
+              Endpoint "${key[keyName]}" has not been defined in keys or data.`);
+    }
+  });
+};
+
+const checkForNestedBoolean = (obj: any) => {
+  const res = {};
+  function recurse(obj: { [x: string]: any }, current: string) {
+    for (const key in obj) {
+      let value = obj[key];
+      if (value != undefined) {
+        if (value && typeof value === "object") {
+          recurse(value, key);
+        } else {
+          if (typeof value === "boolean") {
+            throw new Error(`[HTTP]
+                      Boolean values are not permitted. Response JSON has property "${key}" with a boolean value. 
+                      Please use, for example, 0 and 1`);
+          }
+        }
+      }
+    }
+  }
+  recurse(obj, null);
 };
--- packages/db/tests/index.js
+++ packages/db/tests/index.js
@@ -1,94 +1,147 @@
 import test from "ava";
 import request from "supertest";
 import express from "express";
-import { QueryTypes, Sequelize } from "sequelize";
+import { Sequelize } from "sequelize";
 import bodyParser from "body-parser";
 import fs from "fs";
 
-process.env.ZENCODE_DIR = "./test/fixtures";
+process.env.ZENCODE_DIR = "./test/db";
 const db = require("..").default;
 const zencode = require("../../core").default;
 
-test.beforeEach((t) => {
+const dbPath1 = 'sqlite://./test/db/db1.db';
+const dbPath2 = 'sqlite://./test/db/db2.db';
+
+test.afterEach((t) => {
   try {
-    fs.unlinkSync("./test/database.test");
-  } catch {}
+    fs.unlinkSync("./test/db/db1.db");
+  } catch { }
   try {
-    fs.unlinkSync("./test/database_tablename.test");
-  } catch {}
+    fs.unlinkSync("./test/db/db2.db");
+  } catch { }
 });
 
-test("Middleware db should exists", (t) => {
+test.beforeEach(async (t) => {
+  const sequelize = new Sequelize(dbPath1);
+  const Table = sequelize.define('firstTable', {
+    result: {
+      type: Sequelize.STRING
+    }
+  }, {
+    freezeTableName: true
+  });
+  await Table.sync();
+  try {
+    const results = Array(5).fill(JSON.stringify({ testkey: "test value" }));
+    for (const result of results) {
+      await Table.create({ result });
+    };
+  } catch (e) {
+    throw e;
+  }
+  sequelize.close();
+});
+
+test.afterEach((t) => {
+  try {
+    fs.unlinkSync("./test/db/db1.db");
+  } catch { }
+  try {
+    fs.unlinkSync("./test/db/db2.db");
+  } catch { }
+});
+
+test("Middleware db should exist", (t) => {
   t.truthy(typeof db, "object");
 });
 
-test.serial("Middleware db should correctly work and save data", async (t) => {
+test.serial("Middleware db should work and response includes variable for db", async (t) => {
   try {
     const app = express();
     app.use(bodyParser.json());
     app.use(db);
     app.use("/*", zencode);
-    const data = { data: { sqlite: "sqlite://./test/database.test" } };
     const res = await request(app)
-      .post("/database")
-      .send(data)
+      .post("/db-test-complex")
       .catch((e) => {
         throw e;
       });
-
     t.is(res.status, 200);
-    t.true(Array.isArray(res.body.array));
-    t.is(res.body.array.length, 5);
-    t.log(res.body.array);
-    const sequelize = await new Sequelize(data.data.sqlite);
-    const saved_result = await sequelize.query("select result from results", {
-      type: QueryTypes.SELECT,
-    });
-    t.log(saved_result);
-
-    const arr = await JSON.parse(saved_result[0].result).array;
-    t.true(Array.isArray(arr));
-    t.is(arr.length, 5);
-    t.deepEqual(arr, res.body.array);
+    t.true(Object.keys(res.body).includes('keypair'), 'could not find "keypair " in response');
+    t.true(Object.keys(res.body).includes('myZenroomStringDictionary'), 'could not find "myZenroomStringDictionary" in response');
   } catch (e) {
     throw e;
   }
 });
 
-test.serial(
-  "Middleware db should correctly work and save data with given tablename",
-  async (t) => {
+test.serial("Middleware db should save the result in db1", async (t) => {
+  try {
     const app = express();
     app.use(bodyParser.json());
     app.use(db);
     app.use("/*", zencode);
-
-    const data = {
-      data: {
-        sqlite: "sqlite://./test/database_tablename.test",
-        tablename: "tablename",
-      },
-    };
-    const sequelize = new Sequelize(data.data.sqlite);
-    const sql =
-      "CREATE TABLE IF NOT EXISTS `tablename` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `result` TEXT, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL);";
-    await sequelize.query(sql);
-    try {
-      const res = await request(app).post("/database_table").send(data);
-      const saved_result = await sequelize.query(
-        "select result from tablename",
-        {
-          type: QueryTypes.SELECT,
-        }
-      );
-      t.is(res.status, 200);
-      t.true(Array.isArray(res.body.array));
-      t.is(res.body.array.length, 5);
-      t.teardown(() => {
-        fs.unlinkSync("./test/database_tablename.test");
+    const res = await request(app)
+      .post("/db-test-complex")
+      .catch((e) => {
+        throw e;
       });
+    const sequelize1 = new Sequelize(dbPath1);
+    const Result1 = sequelize1.define('firstTable', {
+      result: {
+        type: Sequelize.STRING
+      }
+    }, {
+      freezeTableName: true
+    });
+    await Result1.sync();
+    let result;
+    try {
+      let query = await Result1.findByPk(6);
+      query = query.get({ plain: true });
+      result = JSON.parse(query['result']);
     } catch (e) {
-      throw e;
+      result = null;
     }
+    sequelize1.close();
+    t.is(res.status, 200);
+    t.deepEqual(res.body, result, 'The value stored in the database should be the same as the value in the response body');
+  } catch (e) {
+    throw e;
   }
-);
+});
+
+test.serial("Middleware db should save the result of variable given in zencode to db2", async (t) => {
+  try {
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(db);
+    app.use("/*", zencode);
+    const res = await request(app)
+      .post("/db-test-complex")
+      .catch((e) => {
+        throw e;
+      });
+    const sequelize2 = new Sequelize(dbPath2);
+    const Result2 = sequelize2.define('firstCache', {
+      result: {
+        type: Sequelize.STRING
+      }
+    }, {
+      freezeTableName: true
+    });
+    await Result2.sync();
+    let result;
+    try {
+      let query = await Result2.findByPk(1);
+      query = query.get({ plain: true });
+      result = JSON.parse(query['result']);
+    } catch (e) {
+      result = null;
+    }
+    sequelize2.close();
+    t.is(res.status, 200);
+    t.deepEqual(res.body.keypair, result.keypair, 'The value stored in the database should be the same as the value in the response body');
+  } catch (e) {
+    throw e;
+  }
+});
