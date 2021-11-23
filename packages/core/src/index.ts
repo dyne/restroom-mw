5562a2a913ad619d599ed9e9c288b5df9c193a68
Puria Nafisi Azizi <puria@dyne.org>
2021-11-23 18:29:30 +0100

    test linting with hound

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,21 @@
 import { getHooks, hook, initHooks } from "./hooks";
-import { getConf, getData, getKeys, getMessage, getYml, getContractByContractName, getContractFromPath } from "./utils";
+
+import {
+  getConf,
+  getData,
+  getKeys,
+  getMessage,
+  getYml,
+  getContractByContractName,
+  getContractFromPath,
+} from "./utils";
 import { zencode_exec } from "zenroom";
-import { addKeysToContext, addDataToContext, addNextToContext, addConfToContext } from "./context";
+import {
+  addKeysToContext,
+  addDataToContext,
+  addNextToContext,
+  addConfToContext,
+} from "./context";
 import { NextFunction, Request, Response } from "express";
 import * as yaml from "js-yaml";
 import { RestroomResult } from "./restroom-result";
@@ -11,7 +25,6 @@ import { CHAIN_EXTENSION } from "@restroom-mw/utils";
 const functionHooks = initHooks;
 
 export default async (req: Request, res: Response, next: NextFunction) => {
-
   if (req.url === "/favicon.ico") {
     return;
   }
@@ -26,34 +39,37 @@ export default async (req: Request, res: Response, next: NextFunction) => {
 
   /**
    * Centralized api error handling
-   * @param {subject} string subject 
+   * @param {subject} string subject
    * @param {e} NodeJS.ErrnoException error
    */
   const sendError = (subject: string, e: NodeJS.ErrnoException = null) => {
     const exception = e ? e.stack || e.message : "";
     const message = subject + "\n\n\n" + exception;
     if (e.code === "ENOENT") {
-      getMessage(req).then( (mes)=>{
+      getMessage(req).then((mes) => {
         res.status(404).send(mes);
       });
-    } else{
+    } else {
       if (!res.headersSent) {
-          res.status(500).json({
-            zenroom_errors: zenroom_errors,
-            result: zenroom_result,
-            exception: message,
-          });
-          if (e) next(e);
+        res.status(500).json({
+          zenroom_errors: zenroom_errors,
+          result: zenroom_result,
+          exception: message,
+        });
+        if (e) next(e);
       }
     }
   };
 
   /**
    * Centralized api response handling
-   * @param {restroomResult} RestroomResult containing restroom result 
+   * @param {restroomResult} RestroomResult containing restroom result
    * @param {res} Response endpoint response
    */
-  const buildEndpointResponse = (restroomResult: RestroomResult, res: Response) => {
+  const buildEndpointResponse = (
+    restroomResult: RestroomResult,
+    res: Response
+  ) => {
     if (restroomResult?.error) {
       sendError(restroomResult.errorMessage, restroomResult.error);
     } else {
@@ -61,35 +77,48 @@ export default async (req: Request, res: Response, next: NextFunction) => {
     }
   };
 
-  async function resolveRestroomResult(restroomResult: RestroomResult): Promise<RestroomResult> {
+  async function resolveRestroomResult(
+    restroomResult: RestroomResult
+  ): Promise<RestroomResult> {
     return new Promise((resolve) => {
       resolve(restroomResult);
     });
-  };
+  }
 
   /**
    * Function responsible to execute the chain
-   * @param {ymlFile} string containing restroom result 
-   * @param {data} object data object coming from endpoint 
+   * @param {ymlFile} string containing restroom result
+   * @param {data} object data object coming from endpoint
    */
   async function executeChain(fileContents: string, data: any): Promise<any> {
     const ymlContent: any = yaml.load(fileContents);
     const startBlock: string = ymlContent.start;
-  
+
     return await evaluateBlock(startBlock, ymlContent, data);
   }
 
-  const getRestroomResult = async (contractName:string, data:any) : Promise<RestroomResult> => {
+  const getRestroomResult = async (
+    contractName: string,
+    data: any
+  ): Promise<RestroomResult> => {
     const isChain = contractName.split(".")[1] === CHAIN_EXTENSION || false;
     const keys = isChain ? "{}" : getKeys(contractName);
     try {
-      return isChain ? executeChain(getYml(contractName.split(".")[0]), data) : callRestroom(data, keys, getConf(contractName), getContractByContractName(contractName), contractName);
-    } catch (err){
+      return isChain
+        ? executeChain(getYml(contractName.split(".")[0]), data)
+        : callRestroom(
+            data,
+            keys,
+            getConf(contractName),
+            getContractByContractName(contractName),
+            contractName
+          );
+    } catch (err) {
       return await resolveRestroomResult({
-        error: err
+        error: err,
       });
     }
-  }
+  };
 
   async function evaluateBlock(
     block: string,
@@ -98,38 +127,58 @@ export default async (req: Request, res: Response, next: NextFunction) => {
   ): Promise<RestroomResult> {
     console.debug("Current block is " + block);
 
-    const singleContext: BlockContext = { keys: null, data: {}, next: null, conf: "", output:{}};
+    const singleContext: BlockContext = {
+      keys: null,
+      data: {},
+      next: null,
+      conf: "",
+      output: {},
+    };
     try {
-
       addKeysToContext(singleContext, ymlContent.blocks[block]);
       addDataToContext(singleContext, data);
       addConfToContext(singleContext, ymlContent.blocks[block]);
       addNextToContext(singleContext, ymlContent.blocks[block]);
       const zencode = getContractFromPath(block);
-      const restroomResult: any = await callRestroom(singleContext.data, singleContext.keys, singleContext.conf, zencode, block);
-      
+      const restroomResult: any = await callRestroom(
+        singleContext.data,
+        singleContext.keys,
+        singleContext.conf,
+        zencode,
+        block
+      );
+
       if (restroomResult?.error) {
         return await resolveRestroomResult(restroomResult);
       }
       Object.assign(singleContext.output, restroomResult.result);
 
-      if(!singleContext?.next){
+      if (!singleContext?.next) {
         return await resolveRestroomResult({
           result: singleContext?.output,
           status: 200,
         });
       }
-    } catch (err){
+    } catch (err) {
       return await resolveRestroomResult({
         error: err,
-        errorMessage: `[CHAIN EXECUTION ERROR FOR CONTRACT ${block}]`
+        errorMessage: `[CHAIN EXECUTION ERROR FOR CONTRACT ${block}]`,
       });
     }
-    return await evaluateBlock(singleContext.next, ymlContent, singleContext.output);
+    return await evaluateBlock(
+      singleContext.next,
+      ymlContent,
+      singleContext.output
+    );
   }
 
-  async function callRestroom(data: string, keys: string, conf:string, zencode:Zencode, contractPath:string): Promise<RestroomResult>{
-    
+  async function callRestroom(
+    data: string,
+    keys: string,
+    conf: string,
+    zencode: Zencode,
+    contractPath: string
+  ): Promise<RestroomResult> {
     let restroomResult: RestroomResult = {};
 
     try {
@@ -143,7 +192,12 @@ export default async (req: Request, res: Response, next: NextFunction) => {
         .then(async ({ result }) => {
           zenroom_result = result;
           result = JSON.parse(result);
-          await runHook(hook.SUCCESS, { result, zencode, zenroom_errors, outcome: restroomResult });
+          await runHook(hook.SUCCESS, {
+            result,
+            zencode,
+            zenroom_errors,
+            outcome: restroomResult,
+          });
           restroomResult.result = result;
           restroomResult.status = 200;
         })
@@ -152,7 +206,11 @@ export default async (req: Request, res: Response, next: NextFunction) => {
         })
         .catch(async (e) => {
           zenroom_errors = e;
-          await runHook(hook.ERROR, { zenroom_errors, zencode, outcome: restroomResult });
+          await runHook(hook.ERROR, {
+            zenroom_errors,
+            zencode,
+            outcome: restroomResult,
+          });
           restroomResult.error = e;
           restroomResult.errorMessage = `[ZENROOM EXECUTION ERROR FOR CONTRACT ${contractPath}]`;
         })
@@ -171,7 +229,7 @@ export default async (req: Request, res: Response, next: NextFunction) => {
   zenroom_result = zenroom_errors = json = "";
   const contractName = req.params["0"];
   let data = getData(req, res);
-  
+
   res.set("x-powered-by", "RESTroom by Dyne.org");
   buildEndpointResponse(await getRestroomResult(contractName, data), res);
 };
@@ -187,3 +245,4 @@ export const {
 } = functionHooks;
 
 export { Restroom } from "./restroom";
+
