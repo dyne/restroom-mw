8ab7e132b312dc70ee5dddd61478855266bc2b2b
Puria Nafisi Azizi <puria@dyne.org>
2021-11-23 17:58:55 -0500

    chore: 👷🏼 Configure hound with eslint to not bother on es6 (#64)

--- packages/core/tests/context_test.js
+++ packages/core/tests/context_test.js
@@ -1,4 +1,4 @@
-import test from "ava"
+import test from "ava";
 process.env.ZENCODE_DIR = "./test/fixtures";
 
 const {
@@ -9,57 +9,82 @@ const {
 } = require("../dist/context");
 
 test("addConfToContext works correctly", (t) => {
+  const ymlContent = { confFile: "not-exists" };
 
-  const ymlContent =  {"confFile": 'not-exists'};
-
-  const singleBlockContext = {data: {"inputData":"iwillbechanged", "otherStuff": "other"}};
+  const singleBlockContext = {
+    data: { inputData: "iwillbechanged", otherStuff: "other" },
+  };
 
   addConfToContext(singleBlockContext, ymlContent);
   t.deepEqual(singleBlockContext.conf, "");
 });
 
 test("addConfToContext works with a file", (t) => {
+  const ymlContent = { confFile: "create-keypair-configuration.conf" };
 
-  const ymlContent =  {"confFile": 'create-keypair-configuration.conf'};
-
-  const singleBlockContext = {data: {"inputData":"iwillbechanged", "otherStuff": "other"}};
+  const singleBlockContext = {
+    data: { inputData: "iwillbechanged", otherStuff: "other" },
+  };
 
   addConfToContext(singleBlockContext, ymlContent);
   t.deepEqual(singleBlockContext.conf, "color=3");
 });
 
 test("addNextToContext works correctly", (t) => {
+  const ymlContent = { next: "next-contract" };
 
-  const ymlContent =  {"next": 'next-contract'};
-
-  const singleBlockContext = {data: {"inputData":"iwillbechanged", "otherStuff": "other"}};
+  const singleBlockContext = {
+    data: { inputData: "iwillbechanged", otherStuff: "other" },
+  };
 
   addNextToContext(singleBlockContext, ymlContent);
-  t.deepEqual(singleBlockContext.next, 'next-contract');
+  t.deepEqual(singleBlockContext.next, "next-contract");
 });
 
 test("addDataToContext works correctly", (t) => {
+  const endpointData = { first_contract: { inputData: "override" } };
 
-  const endpointData =  {"first_contract": {"inputData":"override"}};
-
-  const singleBlockContext = {data: {"inputData":"iwillbechanged", "otherStuff": "other"}};
+  const singleBlockContext = {
+    data: { inputData: "iwillbechanged", otherStuff: "other" },
+  };
 
   addDataToContext(singleBlockContext, endpointData["first_contract"]);
-  t.deepEqual(singleBlockContext.data, {"inputData":"override", "otherStuff": "other"});
+  t.deepEqual(singleBlockContext.data, {
+    inputData: "override",
+    otherStuff: "other",
+  });
 });
 
 test("addKeysToContext works correctly", (t) => {
-
-  const singleBlockContext = {keys: {"userChallenges":"iwillbechanged", "username": "iwillbechanged", "key_derivation": "iwillbechanged" }};
+  const singleBlockContext = {
+    keys: {
+      userChallenges: "iwillbechanged",
+      username: "iwillbechanged",
+      key_derivation: "iwillbechanged",
+    },
+  };
 
   addKeysToContext(singleBlockContext, "create-keypair");
-  t.not(singleBlockContext.keys, {"userChallenges":"iwillbechanged", "username": "iwillbechanged", "key_derivation": "iwillbechanged" });
+  t.not(singleBlockContext.keys, {
+    userChallenges: "iwillbechanged",
+    username: "iwillbechanged",
+    key_derivation: "iwillbechanged",
+  });
 });
 
 test("addKeysToContext works correctly if keys not found", (t) => {
-
-  const singleBlockContext = {keys: {"userChallenges":"iwillbechanged", "username": "iwillbechanged", "key_derivation": "iwillbechanged" }};
+  const singleBlockContext = {
+    keys: {
+      userChallenges: "iwillbechanged",
+      username: "iwillbechanged",
+      key_derivation: "iwillbechanged",
+    },
+  };
 
   addKeysToContext(singleBlockContext, "not-existing-dkeys");
-  t.deepEqual(singleBlockContext.keys, {"userChallenges":"iwillbechanged", "username": "iwillbechanged", "key_derivation": "iwillbechanged" });
+  t.deepEqual(singleBlockContext.keys, {
+    userChallenges: "iwillbechanged",
+    username: "iwillbechanged",
+    key_derivation: "iwillbechanged",
+  });
 });
