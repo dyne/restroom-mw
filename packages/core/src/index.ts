fb756c14539a7371698de5190b2761c27cebe67b
Pasquale Pio Francavilla <pasfranc@gmail.com>
2022-02-07 16:40:43 +0100

    Added check for the path in chains

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -102,6 +102,7 @@ export default async (req: Request, res: Response, next: NextFunction) => {
       }
 
       detectLoop(startBlock, ymlContent);
+      checkAlwaysSamePathInYml(ymlContent);
 
       return await evaluateBlock(startBlock, ymlContent, data);
     } catch (err) {
@@ -127,6 +128,29 @@ export default async (req: Request, res: Response, next: NextFunction) => {
       }
     }
 
+  }
+
+  function checkAlwaysSamePathInYml(
+    ymlContent: any
+  ) {
+    let allFolders: string[] = [];
+    Object.keys(ymlContent?.blocks)
+      .forEach(path=>{
+        if (path.includes("/")){
+          let folder = path.substring(0, path.lastIndexOf("/"));
+          allFolders.push(folder);
+        }
+        Object.keys(ymlContent?.blocks[path]).forEach(prop=>{
+          let value = ymlContent?.blocks[path][prop];
+          if (typeof value === 'string' && value.includes("/")){
+            let folder = value.substring(0, value.lastIndexOf("/"));
+            allFolders.push(folder);
+          }
+        });
+      });
+    if (allFolders.length > 1 && !allFolders.every((val, i, arr) => val === arr[0])){
+      throw new Error(`Permission Denied. The paths in the yml cannot be different`);
+    }
   }
 
   const getRestroomResult = async (
