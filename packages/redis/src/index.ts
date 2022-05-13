135edbaa63a4222f0a6a0de723a0bee171bc02a9
Puria Nafisi Azizi <puria@dyne.org>
2022-05-13 18:04:19 +0000

    feat: create empty redis key when not exists in READ (#157)
    * feat: create empty redis key when not exists in READ
    
    Closes #155 
    
    * feat: Add named params in redis sentences
    
    Closes #156

--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -3,33 +3,30 @@ import { Restroom } from "@restroom-mw/core";
 import { NextFunction, Request, Response } from "express";
 import { zencodeNamedParamsOf } from "@restroom-mw/utils";
 
-/**
- * **CONNECT** `have a redis connection on {}`
- *
- * **WRITE_WITH_KEY** `write data into redis under the key {}`
- *
- * **READ** `read from redis the data under the key {} and save the output into {}";`
- *
- * @constant
- * @default
- */
-const actions = {
-  CONNECT: "have a redis connection on {}",
-  WRITE_WITH_KEY: "write data into redis under the key {}",
-  READ: `read from redis the data under the key {} and save the output into {}`,
+
+enum Action {
+  CONNECT = "have a redis connection on {}",
+  SET = "write data into redis under the key {}",
+  GET = `read from redis the data under the key {} and save the output into {}`,
+  SET_NAMED = "write data into redis under the key named {}",
+  GET_NAMED = `read from redis the data under the key named {} and save the output into {}`
 };
+
 
 export default (req: Request, res: Response, next: NextFunction) => {
   const rr = new Restroom(req, res);
   let client: any = null;
   let getRedisClient: () => any;
+  let namedSet: string = null;
 
   rr.onBefore(async (params: any) => {
-    let { zencode, data } = params;
-    const namedParamsOf = zencodeNamedParamsOf(zencode, data);
+    let { zencode, data, keys } = params;
+    let content = rr.combineDataKeys(data, keys);
+    const namedParamsOf = zencodeNamedParamsOf(zencode, content);
 
-    if (zencode.match(actions.CONNECT)) {
-      const [url] = zencode.paramsOf(actions.CONNECT);
+
+    if (zencode.match(Action.CONNECT)) {
+      const [url] = zencode.paramsOf(Action.CONNECT);
       getRedisClient = (() => {
         const getRedisClientFromUrl = async () => {
           if (client === null) {
@@ -42,20 +39,33 @@ export default (req: Request, res: Response, next: NextFunction) => {
       })()
     }
 
-    if (zencode.match(actions.READ)) {
+    const getFromRedis = async (action: string) => {
       client = client || await getRedisClient();
-      const [key, outputVariable] = namedParamsOf(actions.READ);
+      const [key, outputVariable] = namedParamsOf(action);
+      console.log(key, outputVariable);
+      await client.sendCommand(['SETNX', key, '{}']);
       data[outputVariable] = JSON.parse((await client.get(key)) ?? {});
+    }
+
+    if (zencode.match(Action.GET)) await getFromRedis(Action.GET);
+    if (zencode.match(Action.GET_NAMED)) await getFromRedis(Action.GET_NAMED);
+    if (zencode.match(Action.SET_NAMED)) {
+      const [outputVariable] = namedParamsOf(Action.SET_NAMED);
+      namedSet = outputVariable;
     }
   });
 
   rr.onSuccess(async (args: { result: any; zencode: any }) => {
     const { result, zencode } = args;
+    client = client || await getRedisClient();
 
-    if (zencode.match(actions.WRITE_WITH_KEY)) {
-      client = client || await getRedisClient();
-      const [key] = zencode.paramsOf(actions.WRITE_WITH_KEY);
+    if (zencode.match(Action.SET)) {
+      const [key] = zencode.paramsOf(Action.SET);
       await client.set(key, JSON.stringify(result));
+    }
+
+    if (zencode.match(Action.SET_NAMED)) {
+      await client.set(namedSet, JSON.stringify(result));
     }
   });
 
--- packages/redis/tests/index.ts
+++ packages/redis/tests/index.ts
@@ -21,6 +21,37 @@ test.before(async (t) => {
   t.context = { app: supertest(app), c };
 });
 
+test.serial("Middleware should read non existent key from redis correctly", async (t) => {
+  const { app, c } = t.context;
+  const res = await app.post("/get_non_existent");
+  t.is(res.status, 200, res.text);
+  const result = res.body;
+  const saved = await c.get("myHashkeyThatDoesNotExist");
+  t.true(Object.keys(result).includes("redisResult"));
+  t.deepEqual(JSON.parse(saved), {});
+});
+
+test.serial("Middleware should write on redis correctly with named param", async (t) => {
+  const { app, c } = t.context;
+  const res = await app.post("/set_named");
+  t.is(res.status, 200, res.text);
+  const result = res.body;
+  t.truthy(result.random_object);
+  const saved = await c.get("L1-eth-lastSavedBlock");
+  t.is(saved, JSON.stringify(result));
+});
+
+test.serial("Middleware should read from redis correctly with named param", async (t) => {
+  const { app, c } = t.context;
+  const res = await app.post("/get_named");
+  t.is(res.status, 200, res.text);
+  const result = res.body;
+  const saved = await c.get("L1-eth-lastSavedBlock");
+  t.true(Object.keys(result).includes("redisResult"));
+  t.true(Object.keys(result.redisResult).includes("random_object"));
+  t.is(result.redisResult.random_object, JSON.parse(saved).random_object);
+});
+
 test.serial("Middleware should write on redis correctly", async (t) => {
   const { app, c } = t.context;
   const res = await app.post("/set");
@@ -41,3 +72,5 @@ test.serial("Middleware should read from redis correctly", async (t) => {
   t.true(Object.keys(result.redisResult).includes("random_object"));
   t.is(result.redisResult.random_object, JSON.parse(saved).random_object);
 });
+
+
