98c152977d7b17fcfc1a4db8c760fa9e1f97d6da
Alberto Lerda <albertolerda97@gmail.com>
2022-05-19 13:17:45 +0200

    fix tab vs spaces 😣

--- packages/planetmint/src/actions.ts
+++ packages/planetmint/src/actions.ts
@@ -1,7 +1,7 @@
-export const GENERATEKEY = "create the ed25519 keypair"; 
-export const CONNECT = "have a planetmint endpoint named {}"; 
-export const RETRIEVE = "read the planetmint transaction {} and save the output into {}"; 
-export const ASSET = "create the planetmint transaction to store asset named {}"; 
-export const ASSET_METADATA = "create the planetmint transaction to store asset named {} with metadata {}"; 
-export const SIGNATURE = "create the signed planetmint transaction of {}"; 
+export const GENERATEKEY = "create the ed25519 keypair";
+export const CONNECT = "have a planetmint endpoint named {}";
+export const RETRIEVE = "read the planetmint transaction {} and save the output into {}";
+export const ASSET = "create the planetmint transaction to store asset named {}";
+export const ASSET_METADATA = "create the planetmint transaction to store asset named {} with metadata {}";
+export const SIGNATURE = "create the signed planetmint transaction of {}";
 export const BROADCAST = "ask planetmint to broadcast the {}";
