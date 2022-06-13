e5c181c0fb24ccb409653c238533e4cbb276d548
Alberto Lerda <albertolerda97@gmail.com>
2022-06-13 09:28:42 +0200

    test increment not a number

--- packages/redis/tests/index.ts
+++ packages/redis/tests/index.ts
@@ -107,14 +107,24 @@ test.serial("Middlware inits a counter and increments it", async (t) => {
   const { app, c } = t.context;
   const res = await app.post("/initialize");
   t.is(res.status, 200, res.text);
-  const res1 = await app.post("/increment");
-  t.is(res1.status, 200, res.text);
+  const res1 = await app.post("/increment")
+    .send({keys: {}, data: {key: "atomic-counter"}});
+  t.is(res1.status, 200, res1.text);
   t.is(typeof res1.body.incremented, 'number');
   t.is(res1.body.incremented, 42);
   t.is(res1.body.succ, 43);
-  const res2 = await app.post("/increment");
-  t.is(res2.status, 200, res.text);
+  const res2 = await app.post("/increment")
+    .send({keys: {}, data: {key: "atomic-counter"}});
+  t.is(res2.status, 200, res2.text);
   t.is(typeof res2.body.incremented, 'number');
   t.is(res2.body.incremented, 44);
   t.is(res2.body.succ, 45);
+});
+test.serial("Middlware increments not a number", async (t) => {
+  const { app, c } = t.context;
+  c.set('test:not-number', 'casa')
+  const res = await app.post("/increment")
+    .send({keys: {}, data: {key: "test:not-number"}});
+  t.is(res.status, 500, res.text);
+  t.true(res.body.exception.includes("not an integer"))
 });
