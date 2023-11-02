ba1cbb77ede2db571e8ab260bd542162aef7a498
RebeccaSelvaggini <115783755+RebeccaSelvaggini@users.noreply.github.com>
2023-11-02 16:34:12 +0100

    feat(identity): resolve DID from url and tests (#263)
    Fix [#261](https://github.com/dyne/restroom-mw/issues/261)
    
    ---------
    
    Co-authored-by: Luckydd99 <luca.didomenico99@gmail.com>

---
+++ packages/identity/src/actions.ts
@@ -0,0 +1,11 @@
+export enum Action {
+  /**
+   * `Given I resolve the did 'myDid' from 'myUrl' and save the output into 'myOutput'`<br><br>
+   * Resolve 'myDid' using the resolver in 'myUrl' and save the resolved did json inside 'myOutput'.
+   * By repeting the statement *n* times, it will perform *n* parallel get.
+   * @param {string} myDid - Name of the variable containing the did to be resolved
+   * @param {string} myUrl - Name of the variable containing the url of the resolver
+   * @param {string} myOutput - Name of the variable where the output json will be stored
+   */
+  RESOLVE_DID = "resolve the did {} from {} and save the output into {}",
+}
---
+++ packages/identity/src/index.ts
@@ -0,0 +1,52 @@
+import { Restroom } from "@restroom-mw/core";
+import { ObjectLiteral } from "@restroom-mw/types";
+import { Zencode } from "@restroom-mw/zencode";
+import { NextFunction, Request, Response } from "express";
+import axios from "axios";
+import { Action } from "./actions";
+import { chunks } from "@restroom-mw/http/src/utils";
+
+export default (req: Request, res: Response, next: NextFunction) => {
+  const rr = new Restroom(req, res);
+  let content: ObjectLiteral = {};
+  let promises: Promise<any>[] = [];
+  let outputs: string[] = [];
+
+  rr.onBefore(
+    async (params: {
+      zencode: Zencode;
+      keys: ObjectLiteral;
+      data: ObjectLiteral;
+    }) => {
+      let { zencode, keys, data } = params;
+      content = rr.combineDataKeys(data, keys);
+
+      if (zencode.match(Action.RESOLVE_DID)) {
+        for (let [did, urlName, o] of chunks(zencode.paramsOf(Action.RESOLVE_DID), 3)) {
+          let check_url : URL;
+          let url_resolver : URL;
+          try {
+            check_url = new URL(content[urlName]);
+          } catch (err) {
+            throw new Error("The string " + content[urlName] + " is an invalid URL");
+          }
+          try{
+            url_resolver = new URL(check_url.toString() + "/" + content[did]);
+          } catch(err){
+            throw new Error("The string " + content[did] + " is an invalid URL");
+          }
+          promises.push(axios.get(url_resolver.toString()));
+          outputs.push(o);
+        }
+      }
+      if(promises.length){
+        const result = await Promise.all(promises);
+        result.map((r, i) => {
+          const out = outputs[i];
+          data[out] = r.data;
+        });
+      }
+    }
+  )
+  next();
+}
