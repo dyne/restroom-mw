44b6105adb2407a0bb2376a942bceb59453e0303
matteo-cristino <cristino.matteo97@gmail.com>
2024-02-02 11:11:52 +0100

    fix(identity): 🐛 do not use http module utility functions, but use zencode's one

--- packages/identity/src/index.ts
+++ packages/identity/src/index.ts
@@ -4,7 +4,6 @@ import { Zencode } from "@restroom-mw/zencode";
 import { NextFunction, Request, Response } from "express";
 import axios from "axios";
 import { Action } from "./actions";
-import { chunks } from "@restroom-mw/http/src/utils";
 
 export default (req: Request, res: Response, next: NextFunction) => {
   const rr = new Restroom(req, res);
@@ -22,7 +21,7 @@ export default (req: Request, res: Response, next: NextFunction) => {
       content = rr.combineDataKeys(data, keys);
 
       if (zencode.match(Action.RESOLVE_DID)) {
-        for (let [did, urlName, o] of chunks(zencode.paramsOf(Action.RESOLVE_DID), 3)) {
+        for (let [did, urlName, o] of zencode.chunkedParamsOf(Action.RESOLVE_DID, 3)) {
           let check_url : URL;
           let url_resolver : URL;
           try {
